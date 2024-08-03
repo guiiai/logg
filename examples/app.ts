0d5a48d367ff773fdc2e6c03e538f6970832c415
RainbowBird <git@luoling.moe>
2024-08-03 18:09:32 +0800

    release: v1.0.2
    Signed-off-by: RainbowBird <git@luoling.moe>

---
+++ examples/app.ts
@@ -0,0 +1,11 @@
+import { Format, LogLevel, createLogg, setGlobalFormat, setGlobalLogLevel } from '../src'
+
+setGlobalLogLevel(LogLevel.Debug)
+setGlobalFormat(Format.Pretty) // Otherwise it will output JSON
+
+// Create a logger instance with a specific context
+const log = createLogg('http/request').useGlobalConfig()
+
+log
+  .withField('url', 'https://github.com/guiiai/logg')
+  .log('Welcome to our repository')
