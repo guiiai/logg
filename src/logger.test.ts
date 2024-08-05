003a57935ec946e846759f02c0611e846b85e3db
Neko Ayaka <neko@ayaka.moe>
2024-08-05 22:20:33 +0800

    test: added some basic tests
    Signed-off-by: Neko Ayaka <neko@ayaka.moe>

---
+++ src/logger.test.ts
@@ -0,0 +1,33 @@
+import { describe, expect, it } from 'vitest'
+import { getGlobalFormat, getGlobalLogLevel, setGlobalFormat, setGlobalLogLevel, useLogg } from './logger'
+import { Format, LogLevel } from './types'
+
+describe('logger', () => {
+  it('should log with pretty and debug level', () => {
+    const log = useLogg('test').withFormat(Format.Pretty).withLogLevel(LogLevel.Debug)
+    log.debug('debug')
+    log.log('log')
+    log.warn('warn')
+    log.error('error')
+  })
+
+  it('should log with json and debug level', () => {
+    const log = useLogg('test').withFormat(Format.JSON).withLogLevel(LogLevel.Debug)
+    log.debug('debug')
+    log.log('log')
+    log.warn('warn')
+    log.error('error')
+  })
+
+  it('should be able to set global log level', () => {
+    setGlobalLogLevel(LogLevel.Debug)
+    const level = getGlobalLogLevel()
+    expect(level).toBe(LogLevel.Debug)
+  })
+
+  it('should be able to set global log format', () => {
+    setGlobalFormat(Format.JSON)
+    const format = getGlobalFormat()
+    expect(format).toBe(Format.JSON)
+  })
+})
